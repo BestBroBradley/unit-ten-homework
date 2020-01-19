8ced40bb8898dfa70429d21d1aa3bdb41bd67179
Allison Bradley <allison.bradley@outlook.com>
2020-01-19 15:11:56 -0700

    Added renderHTML functionality

--- app.js
+++ app.js
@@ -10,67 +10,117 @@ const outputPath = path.resolve(__dirname, "output", "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
+
+let status = ""
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 inquirer
     .prompt([
         {
-        message: "What's the Unit Manager's name?",
-        name: "managername"
+            message: "What's the Unit Manager's name?",
+            name: "managername"
         },
         {
-        message: "What's the manager's ID?",
-        name: "managerid"
+            message: "What's the manager's ID?",
+            name: "managerid"
         },
         {
-        message: "What's the manager's email?",
-        name: "manageremail"
+            message: "What's the manager's email?",
+            name: "manageremail"
         },
         {
-        message: "What's the manager's office number?",
-        name: "manageroffnum"
+            message: "What's the manager's office number?",
+            name: "manageroffnum"
         },
         {
-        message: "What type of team member would you like to add?",
-        name: "doNext",
-        type: "list",
-        choices: ["Engineer", "Intern", "I don't want to add anyone else."]
-        },
-    ]) .then (function (response) {
-        if (response.doNext === "Engineer") {
-        console.log ("Selected engineer")
-        .prompt ([
-            {
-            message: "What's the engineer's name?",
-            name: "name"
-            },
-            {
-            message: "What's the engineer's ID?",
-            name: "id"
-            },
-            {
-            message: "What's the engineer's email?",
-            name: "email"
-            },
-            {
-            message: "What's the engineer's GitHub handle?",
-            name: "github"
-            },
-            {
             message: "What type of team member would you like to add?",
             name: "doNext",
             type: "list",
             choices: ["Engineer", "Intern", "I don't want to add anyone else."]
-            },
-        ])
-    
-    } else if (response.doNext === "Intern") {
-            console.log ("Selected intern")
-            } else {
-                console.log ("Selected no more")
-                }
+        },
+    ]).then(function (response) {
+        const newManager = new Manager(response.managername, response.managerid, response.manageremail, response.manageroffnum)
+        employees.push(newManager);
+        status = response.doNext
+        checkStatus()
     })
+
+function checkStatus() {
+    if (status === "Engineer") {
+        askEngineer()
+    } else if (status === "Intern") {
+        askIntern()
+    } else if (status === "I don't want to add anyone else.") {
+        render(employees)
+        return;
+    }
+}
+
+function askIntern() {
+    inquirer.prompt([
+        {
+            message: "What's the intern's name?",
+            name: "name"
+        },
+        {
+            message: "What's the intern's ID?",
+            name: "id"
+        },
+        {
+            message: "What's the interns's email?",
+            name: "email"
+        },
+        {
+            message: "What's the intern's school?",
+            name: "school"
+        },
+        {
+            message: "What type of team member would you like to add?",
+            name: "doNext",
+            type: "list",
+            choices: ["Engineer", "Intern", "I don't want to add anyone else."]
+        },
+    ]).then(function (response) {
+        employees.push(new Intern(response.name, response.id, response.email, response.school));
+        status = response.doNext
+        checkStatus()
+    })
+}
+
+function askEngineer() {
+    inquirer.prompt([
+        {
+            message: "What's the engineer's name?",
+            name: "name"
+        },
+        {
+            message: "What's the engineer's ID?",
+            name: "id"
+        },
+        {
+            message: "What's the engineer's email?",
+            name: "email"
+        },
+        {
+            message: "What's the engineer's GitHub handle?",
+            name: "github"
+        },
+        {
+            message: "What type of team member would you like to add?",
+            name: "doNext",
+            type: "list",
+            choices: ["Engineer", "Intern", "I don't want to add anyone else."]
+        },
+    ]).then(function (response) {
+        employees.push(new Engineer(response.name, response.id, response.email, response.github));
+        status = response.doNext
+        checkStatus()
+    })
+}
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
