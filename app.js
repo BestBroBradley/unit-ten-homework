34e732e1184112a62315c63cf35931d14dbc3bc8
Allison Bradley <allison.bradley@outlook.com>
2020-01-19 13:43:34 -0700

    Added initial inquirer functionality

--- app.js
+++ app.js
@@ -10,9 +10,67 @@ const outputPath = path.resolve(__dirname, "output", "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+inquirer
+    .prompt([
+        {
+        message: "What's the Unit Manager's name?",
+        name: "managername"
+        },
+        {
+        message: "What's the manager's ID?",
+        name: "managerid"
+        },
+        {
+        message: "What's the manager's email?",
+        name: "manageremail"
+        },
+        {
+        message: "What's the manager's office number?",
+        name: "manageroffnum"
+        },
+        {
+        message: "What type of team member would you like to add?",
+        name: "doNext",
+        type: "list",
+        choices: ["Engineer", "Intern", "I don't want to add anyone else."]
+        },
+    ]) .then (function (response) {
+        if (response.doNext === "Engineer") {
+        console.log ("Selected engineer")
+        .prompt ([
+            {
+            message: "What's the engineer's name?",
+            name: "name"
+            },
+            {
+            message: "What's the engineer's ID?",
+            name: "id"
+            },
+            {
+            message: "What's the engineer's email?",
+            name: "email"
+            },
+            {
+            message: "What's the engineer's GitHub handle?",
+            name: "github"
+            },
+            {
+            message: "What type of team member would you like to add?",
+            name: "doNext",
+            type: "list",
+            choices: ["Engineer", "Intern", "I don't want to add anyone else."]
+            },
+        ])
+    
+    } else if (response.doNext === "Intern") {
+            console.log ("Selected intern")
+            } else {
+                console.log ("Selected no more")
+                }
+    })
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
